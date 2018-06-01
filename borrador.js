4adec7776645bede858beb71280f69ee7ea50bec
zettha <39558778+zettha@users.noreply.github.com>
2018-06-01 12:58:35 +0200

    Update borrador.js

--- borrador.js
+++ borrador.js
@@ -1,19 +1,19 @@
 //nav responsve
-window.onload = function() {
-			$("#hamburguesa").on("click", function() {
-				$(".lista").toggle("slow");
-			});
-		}
-//parrafo activo del carousel
-$('.carousel').carousel();
-$('body').on('click','.carousel-control',function() {
-   $(this).closest('.carousel').carousel( $(this).data('slide') );
-}); 
-		$('.carousel').on('slide.bs.carousel', function() {
-			if ($('.active').next().length > 0) {
-				$(".active").toggleClass("active").next().toggleClass("active");
-			} else {
-				$(".contenedor").find(".active").toggleClass("active");
-				$(".contenedor").children(".p0").toggleClass("active");
-			}
-		}) 
+window.onload = function () {
+    $("#hamburguesa").on("click", function () {
+        $(".lista").toggle("slow");
+    });
+}
+
+//velocidad de animacion de carousel
+$(".carousel").carousel({interval: 3000});
+
+//parrafo activo carousel
+$('.carousel').on('slide.bs.carousel', function (ev) {
+    debugger;
+    $(".contenedor > p")
+        .removeClass("active")
+        .eq(ev.to)
+        .addClass("active");
+});
+
