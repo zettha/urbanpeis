ffae634483e3a6f7e92929809ff6e4acb8e4893a
zettha <39558778+zettha@users.noreply.github.com>
2018-06-27 12:51:50 +0200

    Add files via upload

--- peis.js
+++ peis.js
@@ -5,45 +5,46 @@ window.onload = function () {
 }
 
 //velocidad de animacion de carousel
-$(".carousel").carousel({interval: 3000});
+$(".carousel").carousel({
+    interval: 3000
+});
 
 //parrafo activo carousel
 $('.carousel').on('slide.bs.carousel', function (ev) {
-   
+
     $(".cont > p")
         .removeClass("active")
         .eq(ev.to)
         .addClass("active");
 });
 
- // animación
-    $(".animado").click(function(){
-      $(this).toggleClass("animated jello",2000 ) 
-    });
+// animación
+$(".animado").click(function () {
+    $(this).toggleClass("animated jello", 2000)
+});
 
 //nav hamburguesa aparicion del color de fondo
-     $(".fa-bars").on("click", function() {
-        $(this).toggleClass("fondo-nav-hamburguesa");
-    });
+$(".fa-bars").on("click", function () {
+    $(this).toggleClass("fondo-hamburguesa");
+});
 
 // posicionar el nav fijo despues del fondo-inicio
 posicionarMenu();
 
-$(window).scroll(function() {    
+$(window).scroll(function () {
     posicionarMenu();
 });
-
 function posicionarMenu() {
     var altura_del_header = $('.header').outerHeight(true);
     var altura_del_menu = $('.menu').outerHeight(true);
 
-    if ($(window).scrollTop() >= altura_del_header){
+    if ($(window).scrollTop() >= altura_del_header) {
         $('.menu').addClass('fixed');
-          $('#lp').removeClass('aparece');
+        $('.lp').removeClass('aparece');
         $('.wrapper').css('margin-top', (altura_del_menu) + 'px');
     } else {
         $('.menu').removeClass('fixed');
+        $('.lp').addClass('aparece');
         $('.wrapper').css('margin-top', '0');
     }
 }
-
